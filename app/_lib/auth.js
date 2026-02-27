1be27045f3a5ddc7cff857b1fcf60909db07f38e
Calcifer077 <maheshnashier14@gmail.com>
2026-02-27 22:53:04 +0530

    After lecture 477

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -2,6 +2,7 @@
 
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 // For google to work as a provider you need to configure it on the console. You can do that by going to https://console.cloud.google.com/apis/credentials and creating a new OAuth 2.0 Client ID. You will need to set the authorized redirect URI to http://localhost:3000/api/auth/callback/google (or the appropriate URL for your deployment). Once you have created the client ID, you will get a client ID and client secret that you can use in your NextAuth configuration.
 const authConfig = {
@@ -17,6 +18,35 @@ const authConfig = {
     authorized({ auth, request }) {
       // If auth.user return true else return false.
       return !!auth?.user;
+    },
+    // Below callback runs before the actual signup process happens.
+    // Happens after user have put in their details but before the actual sign in process happens.
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          // If there is no existing guest, we can create a new guest in our database.
+          // We need to await here, because if we don't await, the sign in process will continue before the guest is created, and we might run into issues where the user is signed in but there is no corresponding guest in our database.
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+        }
+
+        // If everything went well, return true.
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    // Runs after signin and each time the session is checked.
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+
+      session.user.guestId = guest.id;
+
+      return session;
     },
   },
   pages: {
