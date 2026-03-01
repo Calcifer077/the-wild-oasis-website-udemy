2491630394728ffd7895dde43089d4d703c41fe2
Calcifer077 <maheshnashier14@gmail.com>
2026-03-01 12:37:21 +0530

    After lecture 482

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 // This component is a client component which is trying to render a server component, which will create a conflict.
 
@@ -69,12 +70,27 @@ function UpdateProfileForm({ guest, children }) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
+          <Button />
         </div>
       </form>
     </div>
+  );
+}
+
+function Button() {
+  // 'useFormStatus' is a hook by react, which basically tells us about the status of the form.
+  // One thing to make sure that this hook can only be used inside the component which is being rendered inside a form not for the form itself.
+  // It tells about the status of the form inside which this component is being rendered.
+  // This component needs to be a client componenet.
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
 
