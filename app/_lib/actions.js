e06418fea218f10daee1557cfb11c0d0fa25a4a0
Calcifer077 <maheshnashier14@gmail.com>
2026-03-01 12:29:00 +0530

    After lecture 481

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 // We are basically in the backend here, so you need to verify that the currently logged in user has the right to perform the action, and also you can directly access the database from here.
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -22,11 +23,21 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, countryFlag, nationalID };
 
-  const {data, error} = await supabase.from("guests").update(updateData).eq('id', session.user.guestId).select().single();
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId)
+    .select()
+    .single();
 
   if (error) {
     throw new Error("Guest could not be updated.");
   }
+
+  // The data will not be immediately available on the client side after this action is performed, because of the caching mechanism of next.js. 
+  // It will show stale data from the cache. One can refetch by doing a hard reload, but that is not a good user experience. So we can use 'revalidatePath' to revalidate the cache for a specific path after the action is performed, so that the next time the user visits that path, they will see the updated data.
+  // If you were to specify ('/account') only it will revalidate the cache for the entire '/account' section, which means that all pages under '/account' will show the updated data, but if you specify the exact path ('/account/profile') only that page will show the updated data, and other pages under '/account' will still show the stale data until they are revalidated.
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
